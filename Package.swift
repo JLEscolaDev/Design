e9c2380714144fa5d6dd23246c945994a31e56db
Jose Luis Escolá García <jletrabajoapple@gmail.com>
2025-02-17 11:22:33 +0100

    Keep project structure moving the Package to the main folder and referencing the Design path allowing us to keep the DesignApp but importing Design as a normal SPM package

--- Package.swift
+++ Package.swift
@@ -19,8 +19,9 @@ let package = Package(
         .target(
             name: "Design",
             dependencies: [],
+            path: "Design",
             resources: [
-                .process("Resources/Assets.xcassets"), // Process the Assets file for being able to use it inside the package
+                .process("Resources/Assets.xcassets"),
                 .process("Resources/ice-liquid-1.mp3"),
                 .process("Resources/ice-liquid-2.mp3"),
                 .process("Resources/ice-liquid-3.mp3"),
@@ -29,7 +30,8 @@ let package = Package(
         ),
         .testTarget(
             name: "DesignTests",
-            dependencies: ["Design"]
+            dependencies: ["Design"],
+            path: "Design/Tests"
         ),
     ]
 )
